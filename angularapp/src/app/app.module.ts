32a876de5e1b16f966888957d6555fcde14edd4a
Iamneo <techadmin@iamneo.ai>
2023-08-01 06:48:13 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CurrconvPipe} from './currency.pipe';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
+    CurrconvPipe
     
   ],
   imports: [
